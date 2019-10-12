e32c628ae04257a528ee7a31a78a811a8c1444e1
Darzamat12 <heavylesha@gmail.com>
2019-10-12 14:38:55 +0300

    completed task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,33 @@
+
+
 class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+        this.R=max;
+        this.L=0;
+        this.tempNumber=Math.ceil((max/2));
+        
 
     }
 
     guess() {
+        return (this.tempNumber);
+
 
     }
 
     lower() {
+        this.R=this.tempNumber;
+        this.tempNumber -= Math.floor((this.tempNumber-this.L)/2);
 
     }
 
     greater() {
-
+        this.L=this.tempNumber;
+        this.tempNumber += Math.ceil((this.R-this.tempNumber)/2);
     }
 }
+
 
 module.exports = GuessingGame;
